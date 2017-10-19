c4f4f61f13775c4782f0b92ce84fa6bf83d4c064
Stytan <stytanus@gmail.com>
2017-10-19 13:55:17 +0300

    final

--- DZ_Galery/MainWindow.xaml.cs
+++ DZ_Galery/MainWindow.xaml.cs
@@ -8,19 +8,11 @@
  */
 using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Text;
-using System.Windows;
-using System.Windows.Forms;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Drawing.Imaging;
 using System.IO;
-using System.Windows.Interop;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
 
 namespace DZ_Galery
 {
@@ -30,12 +22,22 @@ namespace DZ_Galery
     public partial class MainWindow : Window
     {
         public List<Image> Images { set; get; }
-        public static int ProgressValue { set; get; }
         public MainWindow()
         {
             InitializeComponent();
-            //ProgressValue = 0;
         }
+        /// <summary>
+        /// Метод для вызова из BitmapConverter, меняет ProgressBar1.Value на заданное значение
+        /// </summary>
+		public void UpdateProgress(int value)
+		{
+			if (CheckAccess())
+                this.ProgressBar1.Value = this.ProgressBar1.Value + value;
+            else
+            {
+                Dispatcher.Invoke(() => { this.ProgressBar1.Value = this.ProgressBar1.Value + value; });
+            }
+		}
         void ButtonOpen_Click(object sender, RoutedEventArgs e)
         {
             FolderBrowserDialog fbd = new FolderBrowserDialog();
@@ -49,6 +51,7 @@ namespace DZ_Galery
                     Files.AddRange(fi);
                 }
                 ProgressBar1.Maximum = Files.Count;
+				ProgressBar1.Value = 0;
                 Images = new List<Image>();
                 try
                 {
@@ -65,36 +68,33 @@ namespace DZ_Galery
                                 FileInfo = fi,
                                 Metadata = meta
                             });
-                        //ProgressValue++;
                     }
                 }
                 catch (Exception ex)
                 {
                     System.Windows.MessageBox.Show(ex.Message);
                 }
-                //ProgressValue = 0;
-                //ProgressBar1.Maximum = Images.Count;
-                //((Image)ListViewImages.SelectedItem).FileInfo
                 ListViewImages.ItemsSource = Images;
-                
             }
         }
 
         private void ListViewImages_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            using (System.Drawing.Image img = System.Drawing.Image.FromFile(Images[ListViewImages.SelectedIndex].FileInfo.FullName, true))
-            {
-                using (Bitmap bitmap = new Bitmap(img))
-                {
-                    ImageOpened.Source = Imaging.CreateBitmapSourceFromHBitmap(
-                        bitmap.GetHbitmap(),
-                        IntPtr.Zero,
-                        Int32Rect.Empty,
-                        BitmapSizeOptions.FromEmptyOptions()
-                    );
-                }
-            }
-            TbTakenData.Text = Images[ListViewImages.SelectedIndex].Metadata.DateTaken;
+			if (ListViewImages.SelectedIndex >= 0) {
+				BitmapImage bmi = new BitmapImage();
+				bmi.BeginInit();
+				bmi.UriSource = new Uri(Images[ListViewImages.SelectedIndex].FileInfo.FullName, UriKind.RelativeOrAbsolute);
+				bmi.EndInit();
+				ImageOpened.Source = bmi;
+				Slider_ValueChanged(sender, new RoutedPropertyChangedEventArgs<double>(0,0));
+				var img = Images[ListViewImages.SelectedIndex];
+				TbTakenData.Text = img.Metadata.DateTaken;
+				TbCamMan.Text = img.Metadata.CameraManufacturer;
+				TbCamModel.Text = img.Metadata.CameraModel;
+				TbFormat.Text = img.Metadata.Format;
+				TbHeight.Text = (bmi.Height * bmi.DpiY / 96).ToString();
+				TbWidth.Text = (bmi.Width * bmi.DpiX / 96).ToString();
+			}
         }
 
         private void ButtonNext_Click(object sender, RoutedEventArgs e)
@@ -103,7 +103,6 @@ namespace DZ_Galery
             {
                 ListViewImages.SelectedIndex++;
             }
-            //Images[ListViewImages.SelectedIndex].Metadata.DateTaken
         }
 
         private void ButtonPrev_Click(object sender, RoutedEventArgs e)
@@ -115,9 +114,9 @@ namespace DZ_Galery
         }
 		void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
 		{
-			ImageOpened.
-			slider.Value;
-			
+			if (ImageOpened != null && ImageOpened.Source != null) {
+				ImageOpened.Width = ImageOpened.Source.Width * slider.Value / 100;
+			}
 		}
     }
 }
